15a8581aed03bf16e0da4136dbf343e2f843c331
林茂昇 <instantaneous108@gmail.com>
2020-01-09 20:42:18 +0800

    PHP_FinalHW

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,8 @@
+{{-- <link rel="stylesheet" type="text/css" href="{{ asset('bower/bootstrap/dist/css/bootstrap.min.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('bower/font-awesome/css/all.min.css') }}" />
+<script type="text/javascript" src="{{ asset('bower/jquery/dist/jquery.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('bower/bootstrap/dist/js/bootstrap.min.js') }}"></script> --}}
+<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" />
+<link href="{{ asset('vendor/fontawesome/css/all.min.css') }}" rel="stylesheet" type='text/css'>
+
+<script type="text/javascript" src="{{ asset('js/app.js') }}" defer></script>
